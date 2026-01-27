af1a01d3209bc698614ad79d06711d24b9fe420d
princekumar-git <pt962441@gmail.com>
2026-01-27 21:45:41 +0530

    superadmin

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import User, Problem, Contest, ForumPost
 
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Problem)
+admin.site.register(Contest)
+admin.site.register(ForumPost)
